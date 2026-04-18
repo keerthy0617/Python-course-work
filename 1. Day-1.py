903170ce698efdce93753307da1f03535b619f89
Keerthy Palakayala <164212226+keerthy0617@users.noreply.github.com>
2026-04-18 14:47:45 +0530

    Delete 1. Day-1.py

--- 1. Day-1.py
+++
@@ -1,6 +0,0 @@
-print("Name: Keerthy")
-print("Course: Python full stack")
-print("Id: CGH3046")
-print("Branch: AIML")
-print("College: Malineni Lakshmaiah Women's Engineering college") 
-print("PASSED year: 2026")
